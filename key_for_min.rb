64cb9b95ed1af9dcc59c118575b75089ec08d0d4
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-09 02:06:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,3 +1,21 @@
 def key_for_min_value(name_hash)
   
+empty = {}
+name_hash = {:blake => 500, :ashley => 2, :adam => 1}
+
+def key_for_min_value(name_hash)
+  smallest_name = nil
+  smallest = 1000000
+
+  name_hash.each do |key, value|
+    if value <= smallest
+      smallest = value
+      smallest_name = key
+    end
+  end
+  smallest_name
+end
+
+
+key_for_min_value(name_hash)
 end
